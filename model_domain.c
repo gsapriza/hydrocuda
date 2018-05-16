7d858dee3962c601dc2ea9f7fbf681ee2b3d0fb5
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-05-16 15:47:47 -0300

    Update model_domain.c

--- model_domain.c
+++ model_domain.c
@@ -5,6 +5,8 @@
 * using c and cuda
 *******************/
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "hydrocuda.h"
 
 void init_domain(){
@@ -12,12 +14,21 @@ void init_domain(){
   //This can be another alternative don't pass as
   extern model_domain options
   extern model_domain domain
+  char caux[15];
   
   if (options.domainModel == 'gridcells'){
-    domain.nx   = 2; //estas opciones estarian dentro de options??
-    domain.ny   = 2;
+    ini_sget(options.config, "ModelDomain", "nx", "%s", caux);
+    domain.nx = atoi(caux);
+    ini_sget(options.config, "ModelDomain", "ny", "%s", caux);
+    domain.ny = atoi(caux);
+    ini_sget(options.config, "ModelDomain", "dx", "%s", caux);
+    domain.dx = atof(caux);
+    ini_sget(options.config, "ModelDomain", "dy", "%s", caux);
+    domain.dy = atof(caux);
+    //domain.nx   = 2; //estas opciones estarian dentro de options??
+    //domain.ny   = 2;
     domain.ntgt = domain.nx*domain.ny;
-    domain.dx   = 1000.0;
-    domain.dy   = 1000.0;
+    //domain.dx   = 1000.0;
+    //domain.dy   = 1000.0;
   }
 }
