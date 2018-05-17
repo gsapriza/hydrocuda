a47a7524e2af2d5abad26faf8c975382666551dd
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-05-17 12:14:30 -0300

    Update meteo_forcing.c

--- meteo_forcing.c
+++ meteo_forcing.c
@@ -7,56 +7,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "hydrocuda.h"
 
 extern model_option options;
 extern model_domain domain;
-//extern moderl_time modeltime;
+//extern model_time modeltime;
 
 void init_forcing(meteo_forcing *forcing){
   
   if (options.meteoModel == 'Pp&Ev'){
     forcing->nmeteo = 2; // Precip and pet
-    typedef struct{
-      char ids[15];
-      //char ev = 'Ev';
-    } meteos_ids;
-    meteo_ids mids[2];
-    mids[0] = "Pp";
-    mids[1] = "Ev";
+    // For loading forcing options
+    meteo_ids *met_ids = (meteo_ids*) malloc(forcing->nmeteo * sizeof(meteo_ids));
+    strcpy(met_ids[0].mids,"Pp");
+    strcpy(met_ids[1].mids,"Ev");
+    // Auxiliar
+    meteo_ids *met_ids_aux = (meteo_ids*) malloc(forcing->nmeteo * sizeof(meteo_ids));
+    strcpy(met_ids_aux[0].mids,"Pp");
+    strcpy(met_ids_aux[1].mids,"Ev");
   }
   else if (options.meteoModel == 'Pp&Penman'){
     forcing->nmeteo = 8; // Precip and variables to Penman pet calc
   }
   // Allocate meteo_info
-  forcing->info = (meteo_info *)malloc(forcing->nmeteo * sizeof(meteo_info));
-  meteo_ids  mids_aux[forcing->nmeteo] = mids;
+  forcing->info = (meteo_info*) malloc(forcing->nmeteo * sizeof(meteo_info));
   // Read variables
   for (int i = 0; i < forcing->nmeteo; i++){
-    ini_sget(options.config, "MeteoModel", strcat(mids[i],"_units"), "%s", forcing->info[i].units);
-    mids[i] = mids_aux[i];
-    ini_sget(options.config, "MeteoModel", strcat(mids[i],"_name"), "%s", forcing->info[i].units);
-    mids[i] = mids_aux[i];
-    ini_sget(options.config, "MeteoModel", strcat(mids[i],"_longname"), "%s", forcing->info[i].units);
-    mids[i] = mids_aux[i];
-    forcing->info[0].units    = 'mm/day'; //Units have to be set from options
-    forcing->info[0].name     = 'precip';
-    forcing->info[0].longname = 'precipitation';
+    ini_sget(options.config, "MeteoModel", strcat(met_ids[i].ids,"_units"), "%s", forcing->info[i].units);
+    strcpy(met_ids[i].ids,met_ids_aux[i].ids);
+    ini_sget(options.config, "MeteoModel", strcat(met_ids[i].ids,"_name"), "%s", forcing->info[i].name);
+    strcpy(met_ids[i].ids,met_ids_aux[i].ids);
+    ini_sget(options.config, "MeteoModel", strcat(met_ids[i].ids,"_longname"), "%s", forcing->info[i].longname);
+    strcpy(met_ids[i].ids,met_ids_aux[i].ids);
+  }
+  //forcing->info[0].units    = 'mm/day'; //Units have to be set from options
+  //forcing->info[0].name     = 'precip';
+  //forcing->info[0].longname = 'precipitation';
   //forcing->info[1].units    = 'mm/day';
   //forcing->info[1].name     = 'evap';
   //forcing->info[1].longname = 'PotEvapotranspiration';
 }
 
-// void allocate_forcing(meteo_forcing *forcing, model_time *modeltime){
-//   // Allocation and setting to zero
-//   // We shoud distingush between ntime block allocating,
-//   // All time steps or in each time step
-
-//   if (options.meteoModel == 'Pp&Ev'){
-//     forcing->precip = (*float)*malloc(sizeof(float)*domain.ntgt*modeltime.nt);
-//     forcing->pet    = (*float)*malloc(sizeof(float)*domain.ntgt*modeltime.nt);
-//   }
-// }
+void allocate_forcing(meteo_forcing *forcing, model_time *modeltime){
+  // Allocation and setting to zero
+  // We shoud distingush between ntime block allocating,
+  // All time steps or in each time step
+  
+  for (int i = 0; i < forcing->nmeteo; i++){
+    forcing->info.meteo = (float*) malloc(sizeof(float) * domain.ntgt * modeltime->nt);
+    //forcing->precip = (float*)*malloc(sizeof(float)*domain.ntgt*modeltime.nt);
+    //forcing->pet    = (float*)*malloc(sizeof(float)*domain.ntgt*modeltime.nt);
+  }
+}
 // 
 // void read_forcing_netdf4(){
 //
