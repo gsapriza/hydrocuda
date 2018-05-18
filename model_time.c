0fabc237b0d2cf8a6af896d3ebfe25e8a7d2387a
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-05-18 09:38:53 -0300

    Create model_time.c

---
+++ model_time.c
@@ -0,0 +1,11 @@
+/**************
+* HydroCuda model - model_time
+*
+* Set of basic function to init, set, read of timing options
+* using c and cuda
+****************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "hydrocuda.h"
+
