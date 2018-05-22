fd260bea2c68d5e1ef4e5bbd8de84cb7b2a2889e
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-05-22 09:56:14 -0300

    Create init_states.c

---
+++ init_states.c
@@ -0,0 +1,19 @@
+/**************
+* HydroCuda model - model_time
+*
+* Set of basic function to init, set, read of states options
+* using c and cuda
+****************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "hydrocuda.h"
+
+extern model_option options;
+extern model_domain domain;
+extern model_time modeltime;
+
+void init_states(){
+  
+  
+}
