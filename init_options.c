82088138180ef793006966dd31e47886e14c482f
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-05-22 09:48:30 -0300

    Update init_options.c

--- init_options.c
+++ init_options.c
@@ -4,15 +4,18 @@
 * Set of hydrologic models to simulate the hydrological model response
 * using c and cuda
 *******************/
-#include "hydrocuda.h"
+
 #include <stdlib.h>
+#include "hydrocuda.h"
 
 void init_modeloptions(){
   
   extern model_option options; //call options decleared in main
+  // optns.model =  'HBV';
   // optns.domainModel =  'gridcells';
   // optns.meteoModel  = 'Pp&Ev';
   // optns.istimeblock = 1;
+  ini_sget(options.config, "Model", "model", "%s", options.model);
   ini_sget(options.config, "ModelDomain", "support_type", "%s", options.domainModel);
   ini_sget(options.config, "MeteoModel", "forcing_type", "%s",  options.meteoModel);
   char caux[15]; //auxiliar to read integer
