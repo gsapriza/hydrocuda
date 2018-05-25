cf8234d5b1fa406c5716de5f119556970670b65b
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-05-25 13:52:42 -0300

    Create auxiliar_functions.c

---
+++ auxiliar_functions.c
@@ -0,0 +1,21 @@
+
+/**************
+* HydroCuda model
+*
+* Set of hydrologic models to simulate the hydrological model response
+* using c and cuda
+***************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "hydrocuda.h"
+
+extern model_option options;
+extern model_domain domain;
+extern model_time modeltime;
+
+// Initialize parameter, states and flows
+void init_psf(model_vars modelvars){
+  
+  
+}
