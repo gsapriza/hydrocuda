b60a3b5e9ad2125f5cea847a5b29e31f474b878d
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-05-25 15:19:25 -0300

    Update auxiliar_functions.c

--- auxiliar_functions.c
+++ auxiliar_functions.c
@@ -2,7 +2,7 @@
 /**************
 * HydroCuda model
 *
-* Set of hydrologic models to simulate the hydrological model response
+* Set of auxiliar functions simulate hydrological models response
 * using c and cuda
 ***************/
 
@@ -10,16 +10,16 @@
 #include <stdlib.h>
 #include "hydrocuda.h"
 
-extern model_option options;
-extern model_domain domain;
-extern model_time modeltime;
+// extern model_option options;
+// extern model_domain domain;
+// extern model_time modeltime;
 
 // Initialize parameter, states and flows
 void getindx(model_vars mv, char *c){
   for (int i = 0; i < mv.nvars ; i++){
     if (c == mv.name){
-      int j = i;
+      break;
     }
   }
-  return j;
+  return i;
 }
