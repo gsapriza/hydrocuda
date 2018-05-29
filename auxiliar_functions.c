8b283fd8d60e560b634f2fb1d0a332e76c318cbe
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-05-29 12:17:19 -0300

    Update auxiliar_functions.c

--- auxiliar_functions.c
+++ auxiliar_functions.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "hydrocuda.h"
 
 // extern model_option options;
@@ -16,8 +17,9 @@
 
 // Get index of different variables
 int getindx(model_vars mv, char *c){
-  for (int i = 0; i < mv.nvars ; i++){
-    if (c == mv.info[i].name){
+  int i;
+  for ( i = 0; i < mv.nvars; i++){
+    if (strcmp( c, mv.info[i].name) == 0){
       break;
     }
   }
