9e3ca5f9dfd0a5744763e81a6519c962b9ea1109
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-06-11 15:28:00 -0300

    Create read_netcdf.c

---
+++ read_netcdf.c
@@ -0,0 +1,20 @@
+/**************
+* HydroCuda model
+*
+* Set of hydrologic models to simulate the hydrological model response
+* using c and cuda
+***************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hydrocuda.h"
+
+extern model_option options;
+extern model_domain domain;
+extern model_time modeltime;
+
+// Initialize parameter, states and flows
+void init_hbv(model_vars modelvars){
+  
+}
