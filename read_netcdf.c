126ffa6d3f227c07f8a1110bd0d820dbebf5bb5b
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-06-18 15:32:24 -0300

    Update read_netcdf.c

--- read_netcdf.c
+++ read_netcdf.c
@@ -5,6 +5,7 @@
 * using c and cuda
 ***************/
 
+#include <netcdf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,7 +15,13 @@ extern model_option options;
 extern model_domain domain;
 extern model_time modeltime;
 
-// Initialize parameter, states and flows
-void init_hbv(model_vars modelvars){
+// Reaf NetCDF files
+void read_netcdf(model_vars modelvars, size_t start, size_t count, float *var){
   
+  int nc_id, nc_error, var_id;
+  int time_dim = count[0];
+  //float var[time_dim][domain.nx][domain.ny];
+  nc_error = nc_open(location, NC_NOWRITE, &nc_id);
+  nc_error = nc_inq_varid(nc_id, name, &var_id);
+  nc_error = nc_get_vara_float(nc_id, var_id, start, count, &var[0][0][0]);
 }
