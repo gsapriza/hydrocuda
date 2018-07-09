0824345726ad034a7e0ef50c3e1759bb3ab2e3ae
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-07-09 12:56:34 -0300

    Update auxiliar_functions.c

--- auxiliar_functions.c
+++ auxiliar_functions.c
@@ -6,6 +6,7 @@
 * using c and cuda
 ***************/
 
+#include <netcdf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,8 +16,8 @@
 // extern model_domain domain;
 // extern model_time modeltime;
 
-// Get index of different variables
 int getindx(model_vars mv, char *c){
+  // Get index of different variables
   int i;
   for ( i = 0; i < mv.nvars; i++){
     if (strcmp( c, mv.info[i].name) == 0){
@@ -26,3 +27,14 @@ int getindx(model_vars mv, char *c){
   }
   return i;
 }
+
+
+int read_netcdf(char *location, char *name, size_t *start, size_t *count, float *var){
+  // Reaf NetCDF files
+  int nc_id, nc_error, var_id;
+  nc_error = nc_open(location, NC_NOWRITE, &nc_id);
+  nc_error = nc_inq_varid(nc_id, name, &var_id);
+  nc_error = nc_get_vara_float(nc_id, var_id, start, count, &var[0]);
+  return nc_error;
+}
+
