7c6afa824a790ca53c96b50dd5c4a1cd63e434c7
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-07-09 09:58:29 -0300

    Update read_netcdf.c

--- read_netcdf.c
+++ read_netcdf.c
@@ -11,15 +11,15 @@
 #include <string.h>
 #include "hydrocuda.h"
 
-extern model_option options;
-extern model_domain domain;
-extern model_time modeltime;
+// extern model_option options;
+// extern model_domain domain;
+// extern model_time modeltime;
 
 
 int read_netcdf(char *location, char *name, size_t *start, size_t *count, float *var){
   // Reaf NetCDF files
   int nc_id, nc_error, var_id;
-  nc_error = nc_open(modelvars.info[i].location, NC_NOWRITE, &nc_id);
+  nc_error = nc_open(location, NC_NOWRITE, &nc_id);
   nc_error = nc_inq_varid(nc_id, name, &var_id);
   nc_error = nc_get_vara_float(nc_id, var_id, start, count, &var[0]);
   return nc_error;
