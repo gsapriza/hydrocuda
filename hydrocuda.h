2b73b878d4f01b3980f4174254b8f9aab31dd953
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-07-16 14:54:40 -0300

    Update hydrocuda.h

--- hydrocuda.h
+++ hydrocuda.h
@@ -86,7 +86,7 @@ void init_times();
 void init_hbv();
 void allocate_hbv();
 void free_hbv();
-void read_hbv_netdf();
+void read_hbv_netcdf();
 void update_param_hbv();
 void compute_hbv();
 void compute_hbv_cuda();
@@ -94,7 +94,7 @@ void compute_hbv_cuda();
 // Meteorological forcing
 void init_forcing();
 void allocate_forcing();
-void read_forcing_netdf();
+void read_forcing_netcdf();
 // void update_forcing();
 void free_forcing();
 
