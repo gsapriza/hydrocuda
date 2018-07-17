8749ff19795ea696408cadb3767db96a95d90b0e
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-07-17 17:57:17 -0300

    Update hydrocuda_drive.c

--- hydrocuda_drive.c
+++ hydrocuda_drive.c
@@ -16,34 +16,46 @@ model_time modeltime;
 
 int main (int argc, char *argv[])
 {
-    // Variable declraration
-    model_vars forcing;
-    model_vars modelvars;
-
-    // Model Initialization
-    if(argc < 2){
-      printf("Usage : ./a.out <filename>");
-      exit(0);
-    }
-    options.config = ini_load(argv[1]);
-
-    init_modeloptions();
-    init_domain();
-    init_times();
-
-    //
-    init_hbv(&modelvars);
-    init_forcing(&forcing); //init forcing
-    //Aca es donde iteramos por bloques llamando a la f
-    // evaluate_model()
-    // for(itblock = 0; itblock<nblock,itblock++){
-    //     //esta funcion tiene que ser la que se aplica cuda
-    //     compute_waterbalance();
-    //     compute_transit();
-    //     //Leemos el siguient bloque de datos temporales
-    //     update_forcing();
-    // }
-
-    ini_free(options.config);
-    return 0;
+  // Variable declraration
+  model_vars forcing;
+  model_vars modelvars;
+  
+  // Model Initialization
+  if (argc < 2){
+    printf("Usage : ./a.out <filename>");
+    exit(0);
+  }
+  //  To read .ini files
+  options.config = ini_load(argv[1]);
+  // Initialize basic model options
+  init_modeloptions();
+  init_domain();
+  init_times();
+  
+  // Init data series and model
+  //if (strcmp(options.hydroModel,"HBV") == 0){
+  //  init_hbv(modelvars);
+  //  void allocate_hbv(&modelvars);
+  //}
+  init_hbv(&modelvars);
+  init_forcing(&forcing);
+  // Allocate information
+  allocate_hbv(&modelvars);
+  allocate_forcing(&forcing);
+  // Aca es donde iteramos por bloques llamando a la f
+  // evaluate_model()
+  for(int itblock = 0; itblock < modeltime.nt, itblock++){
+    update_forcing(&forcing, i);
+    compute_hbv(&modelvars, &forcing);
+    // Esta funcion tiene que ser la que se aplica cuda
+    //compute_waterbalance();
+    //compute_transit();
+    // Leemos el siguient bloque de datos temporales
+    //update_forcing();
+  }
+  // Free information
+  ini_free(options.config);
+  free_forcing(&forcing);
+  free_hbv(&modelvars);
+  return 0;
 }
