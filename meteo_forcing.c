2231f9472df3b2b285c9202db00cc767e8e0d464
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-07-23 17:51:36 -0300

    Update meteo_forcing.c

--- meteo_forcing.c
+++ meteo_forcing.c
@@ -14,25 +14,25 @@ extern model_option options;
 extern model_domain domain;
 extern model_time modeltime;
 
-void init_forcing(model_vars forcing){
+void init_forcing(model_vars *forcing){
   
   aux_ids *met_ids;
   if (strcmp(options.meteoModel, "Pp&Etp") == 0){
-    forcing.nvars = 2; // Precip and pet
+    forcing->nvars = 2; // Precip and pet
     // For loading forcing options
-    met_ids = (aux_ids*) malloc(forcing.nvars * sizeof(aux_ids));
+    met_ids = (aux_ids*) malloc(forcing->nvars * sizeof(aux_ids));
     strcpy(met_ids[0].ids,"pp"); // Precipitation
     strcpy(met_ids[1].ids,"etp"); // Potential evotranspiration
   }
   else if (strcmp(options.meteoModel, "Pp&Penman") == 0){
-    forcing.nvars = 8; // Precip and variables to Penman pet calc
-    met_ids = (aux_ids*) malloc(forcing.nvars * sizeof(aux_ids));
+    forcing->nvars = 8; // Precip and variables to Penman pet calc
+    met_ids = (aux_ids*) malloc(forcing->nvars * sizeof(aux_ids));
   }
   // Allocate auxiliar
-  aux_ids *met_ids_aux = (aux_ids*) malloc(forcing.nvars * sizeof(aux_ids));
+  aux_ids *met_ids_aux = (aux_ids*) malloc(forcing->nvars * sizeof(aux_ids));
   // Allocate meteo_info
-  forcing.info = (vars_info*) malloc(forcing.nvars * sizeof(vars_info));
-  for (int i = 0; i < forcing.nvars; i++){
+  forcing->info = (vars_info*) malloc(forcing->nvars * sizeof(vars_info));
+  for (int i = 0; i < forcing->nvars; i++){
     // Fill auxiliar
     strcpy(met_ids_aux[i].ids,met_ids[i].ids);
     // Read variables
@@ -41,19 +41,19 @@ void init_forcing(model_vars forcing){
     ini_sget(options.config, "MeteoModel", strcat(met_ids[i].ids,"_location"), "%s", forcing.info[i].location);
     strcpy(met_ids[i].ids,met_ids_aux[i].ids);
     // Variable known declaration
-    strcpy(forcing.info[i].name,met_ids_aux[i].ids);
-    forcing.info[i].indx = i;
-    forcing.info[i].tipe = 4;
+    strcpy(forcing->info[i].name,met_ids_aux[i].ids);
+    forcing->info[i].indx = i;
+    forcing->info[i].tipe = 4;
   }
 }
 
-void allocate_forcing(model_vars forcing){
+void allocate_forcing(model_vars *forcing){
   // Allocation and setting to zero
   // We shoud distingush between ntime block allocating,
   // All time steps or in each time step
   
-  for (int i = 0; i < forcing.nvars; i++){
-    forcing.info[i].vars = (float*) malloc(sizeof(float) * domain.ntgt);
+  for (int i = 0; i < forcing->nvars; i++){
+    forcing->info[i].vars = (float*) malloc(sizeof(float) * domain.ntgt);
   }
 }
 
@@ -83,14 +83,13 @@ void update_forcing(model_vars *forcing, int i){
   size_t start[] = {i, 0, 0};
   size_t count[] = {1, domain.nx, domain.ny};
   for (int i = 0; i < forcing->nvars; i++){
-    read_forcing_netcdf(&forcing, start, count);
+    read_forcing_netcdf(forcing, start, count);
   }
 }
 
-void free_forcing(model_vars forcing){
+void free_forcing(model_vars *forcing){
   // Free forcing matrixs
-  for (int i = 0; i < forcing.nvars; i++){
-    free(forcing.info[i].vars);
+  for (int i = 0; i < forcing->nvars; i++){
+    free(forcing->info[i].vars);
   }
-
 }
