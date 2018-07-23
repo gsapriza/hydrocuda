5ca1752c40751cc30bba43c8f961636dad441978
rafaelbanega <38990100+rafaelbanega@users.noreply.github.com>
2018-07-23 17:47:26 -0300

    Update auxiliar_functions.c

--- auxiliar_functions.c
+++ auxiliar_functions.c
@@ -12,16 +12,12 @@
 #include <string.h>
 #include "hydrocuda.h"
 
-// extern model_option options;
-// extern model_domain domain;
-// extern model_time modeltime;
-
-int getindx(model_vars mv, char *c){
+int getindx(model_vars *mv, char *c){
   // Get index of different variables
   int i;
-  for ( i = 0; i < mv.nvars; i++){
-    if (strcmp( c, mv.info[i].name) == 0){
-      i = mv.info[i].indx;
+  for ( i = 0; i < mv->nvars; i++){
+    if (strcmp( c, mv->info[i].name) == 0){
+      i = mv->info[i].indx;
       break;
     }
   }
@@ -34,7 +30,7 @@ int read_netcdf(char *location, char *name, size_t *start, size_t *count, float 
   int nc_id, nc_error, var_id;
   nc_error = nc_open(location, NC_NOWRITE, &nc_id);
   nc_error = nc_inq_varid(nc_id, name, &var_id);
-  nc_error = nc_get_vara_float(nc_id, var_id, start, count, &var[0]);
+  nc_error = nc_get_vara_float(nc_id, var_id, start, count, var);
   return nc_error;
 }
 
